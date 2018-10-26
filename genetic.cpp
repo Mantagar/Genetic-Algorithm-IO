f3ce84257c5c4a792b26bc5084acbb72554402fb
Marcin Radzio <enevyn@gmail.com>
2018-10-26 18:31:57 +0200

    first commit

---
+++ genetic.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+class Genetic
+{
+public:
+	Genetic(int populationSize) {
+
+	}
+	void run() {
+		Init();
+		Eval();
+		do {
+			Select();
+			Crossover();
+			Mutate();
+			Eval()
+		} while ShouldContinue();
+
+	}
+private:
+	void Init() {
+
+	}
+	void Eval() {
+
+	}
+	bool ShouldContinue() {
+		return false;
+	}
+	void Select() {
+
+	}
+	void Crossover() {
+
+	}
+	void Mutate() {
+
+	}
+};
+
+int main() {
+	Genetic instance(0);
+	instance.run();
+
+	return 0;
+}
