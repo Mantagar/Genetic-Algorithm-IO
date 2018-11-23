95fbd7dcc030130ff9087770134ab5b7607759ae
Marcin Radzio <plgmantagar@zeus.cyfronet.pl>
2018-11-23 12:59:15 +0100

    script

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
-#SBATCH -J GeneticTest
-#SBATCH -N 8
-#SBATCH --ntasks-per-node=8
-#SBATCH --time=00:10:00 
+#SBATCH -J Genetic
+#SBATCH -N 5
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=10:00:00 
 #SBATCH -A plgmantagar2018a
-#SBATCH -p plgrid-short
+#SBATCH -p plgrid-long
 #SBATCH --output="output.out" 
 
 srun /bin/hostname
@@ -13,4 +13,4 @@ module add plgrid/tools/openmpi/1.10.0-gnu-4.9.2-ib
  
 cd $SLURM_SUBMIT_DIR
  
-mpiexec ./genetic.out 1000 25 0.01 2
+mpiexec ./genetic.out 2000 25 0.01 2
